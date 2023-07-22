2c17fb84bbebf95eccf111f44934445c1cdfd2ea
Sthéfani Martinelli <sthefani_martinelli@hotmail.com>
2023-07-21 21:20:02 -0300

    Corrige erro na estrutura dos schemas e na exportação das models, corrige erro de importação no controller

--- backend/src/models/CartModel.js
+++ backend/src/models/CartModel.js
@@ -1,26 +1,24 @@
 const mongoose = require("mongoose");
+const { productSchema } = require("./ProductModel");
 
-const { Schema } = mongoose;
-
-const productSchema = require("./ProductModel");
-
-const cartSchema = new Schema({
-  customerEmail: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  customerName: {
-    type: String,
-    required: true,
-  },
-  products: [ productSchema ],
-  totalPrice: {
-    type: Number,
-    required: true,
-  },
-},
-{ timestamps: true }
+const cartSchema = new mongoose.Schema(
+    {
+        customerEmail: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        customerName: {
+            type: String,
+            required: true,
+        },
+        products: [productSchema],
+        totalPrice: {
+            type: Number,
+            required: true,
+        },
+    },
+    { timestamps: true },
 );
 
 const Cart = mongoose.model("Cart", cartSchema);
--- backend/src/models/ProductModel.js
+++ backend/src/models/ProductModel.js
@@ -1,27 +1,24 @@
 const mongoose = require("mongoose");
-
 const { Schema } = mongoose;
 
 const productSchema = new mongoose.Schema({
-  productId: {
-    type: Schema.Types.ObjectId,
-    ref: "Product",
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  quantity: {
-    type: Number,
-    default: 1,
-    required: true,
-  },
-  notes: {
-    type: String,
-  },
+    // productId: {
+    //     type: Schema.Types.ObjectId,
+    //     required: true,
+    // },
+    price: {
+        type: Number,
+        required: true,
+    },
+    quantity: {
+        type: Number,
+        default: 1,
+        required: true,
+    },
+    notes: {
+        type: String,
+    },
 });
 
 const Product = mongoose.model("Product", productSchema);
-
-module.exports = productSchema;
+module.exports = { productSchema, Product };
