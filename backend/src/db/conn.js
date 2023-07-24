ca627419e14a8cbc2e3f06e86ce66cdf28b4cd3f
Sthéfani Martinelli <sthefani_martinelli@hotmail.com>
2023-07-24 16:58:05 -0300

    Altera arquivo de entrada do projeto e muda as variáveis de ambiente do arquivo connection

--- backend/src/db/conn.js
+++ backend/src/db/conn.js
@@ -16,23 +16,23 @@ require('dotenv').config();
 //   }
 // }
 
-const MONGO_USER = process.env.MONGO_INITDB_ROOT_USERNAME;
-const MONGO_PASSWORD = process.env.MONGO_INITDB_ROOT_PASSWORD;
+const MONGO_USER = process.env.DB_USER;
+const MONGO_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
+const DB_NAME = process.env.DB_NAME;
 
 async function main() {
-  try {
-    // await mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@enafood_db:27017/database`, {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    // });
-    await mongoose.connect("mongodb://enafood_db:27017/database", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Conectado ao banco!");
-  } catch (error) {
-    console.log(`Erro: ${error}`);
-  }
+    const MONGO_URL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+    try {
+        await mongoose.connect(MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Conectado ao banco!");
+    } catch (error) {
+        console.log(`Erro: ${error}`);
+    }
 }
 
 module.exports = main;
